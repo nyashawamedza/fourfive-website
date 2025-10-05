31cf517222742f2f6e2c94278858aee255c8c219
nwamedza <nyashawamedza@Nyashas-MacBook-Air.local>
2025-10-05 23:56:38 +0200

    Fix Railway deployment: use npx serve instead of global install

--- start.sh
+++ start.sh
@@ -6,9 +6,5 @@ npm ci
 # Build the production bundle
 npm run build
 
-# Install a simple HTTP server to serve static files
-npm install -g serve
-
-# Serve the built files from the dist directory
-# PORT is provided by Railway/Railpack
-serve -s dist -l ${PORT:-3000}
+# Install serve locally (not globally to avoid permission issues)
+npx serve -s dist -l ${PORT:-3000}
